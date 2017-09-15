971cb031e5d9f779a9f76aab3725809eadbfff18
tibhar940 <tibhar940@gmail.com>
2017-09-15 13:19:26 +0300

    Update testsn

--- tests/test_ISS.trades.py
+++ tests/test_ISS.trades.py
@@ -7,31 +7,37 @@ pd.set_option('display.width', 1000)
 
 from ISS.trades import trades, Trades
 
-# t = trades(engine='stock', market='shares')
-# print(t.shape)
-# print(t.head())
-#
-# t = trades(engine='stock', market='shares', limit=100)
-# print(t.shape)
-# print(t.head())
-#
-# t = trades(engine='stock', market='shares', boardid='TQBR')
-# print(t.shape)
-# print(t.head())
-#
-# t = trades(engine='stock', market='shares', securityid='SBER')
-# print(t.shape)
-# print(t.head())
+t = trades(engine='stock', market='shares')
+print(t.shape)
+print(t.head())
+
+t = trades(engine='stock', market='shares', limit=100)
+print(t.shape)
+print(t.head())
+
+t = trades(engine='stock', market='shares', boardid='TQBR')
+print(t.shape)
+print(t.head())
+
+t = trades(engine='stock', market='shares', securityid='SBER')
+print(t.shape)
+print(t.head())
+
+T = Trades(engine='stock', market='shares')
+print(T)
+T.load()
+print(T)
+print(T.trades.head())
 
 T = Trades(engine='stock', market='shares', securityid='LKOH')
 print(T)
 T.load()
-print(T.trades.shape)
+print(T)
 print(T.trades.head())
 
 T = Trades(engine='stock', market='shares', boardid='TQBR')
 print(T)
 T.load()
-print(T.trades.shape)
+print(T)
 print(T.trades.head())
-print(T.trades['secid'].value_counts().head(10))
+print(T.trades.info())
